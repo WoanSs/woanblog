300e825bad66b7716f6f41baedffd63451d6acfa
Woan <woanshanks267@gmail.com>
2025-04-27 03:26:14 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Woan-4ab403a0499b421e8c976bde96ae3ef0',
+  rootNotionPageId: '1e17d0f2a58780a2beb9f2a923eddabc',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)

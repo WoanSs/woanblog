8534cd07c0243b4bddd4f9e294f6b8d76d923d73
Woan <woanshanks267@gmail.com>
2025-04-27 02:58:24 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Woan-4ab403a0499b421e8c976bde96ae3ef0?pvs=4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Woan Blog',
+  domain: 'woanblog-woans-projects.vercel.app',
+  author: 'Woan',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
